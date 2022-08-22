87ff832ab90fbf5f0aa151ce9de700c23d848764
R3dSpyder <carlmckeown1@gmail.com>
2022-08-22 12:37:52 +0100

    t

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,14 @@
 const getAPI = require("../mvc/controllers/getAPI.js");
-const db = require("../db/connection.js").default;
+const db = require("../db/connection.js");
 const express = require("express");
 const router = express.Router();
-// const cookieParser = require("cookie-parser");
-// const session = require("express-session");
-const { application } = require("express");
+const cors = require("cors");
 
+const { application } = require("express");
+const { app } = require("../main_app.js");
+app.use(cors());
 router.route("/").get(getAPI);
-// app.use(cookieParser()); //user preferences
-// app.use(
-//   session({
-//     secret: "anystring",
-//     saveUninitialized: true,
-//     resave: true,
-//     store: db,
-//   })
-// ); //secrets session
-//error handling for path
+
 require("../error_handling/wrongPathErrors");
 
 module.exports = router;
